acf1d587bec56b7bf4ed8de5721d44964b4f7775
benwato <benwato12@gmail.com>
2021-10-27 23:02:50 +0100

    Updated styling for mode and category page

--- src/pages/CreateRoom/index.js
+++ src/pages/CreateRoom/index.js
@@ -23,7 +23,7 @@ export default function CreateRoom() {
    
       !redirect ? <div className="d-flex align-items-center mt-5">
       <Container>
-          <h1>select game mode</h1>
+          <h1>Select Game Mode</h1>
         {/* <Container className= 'customBox border border-dark'>
                     <Row>
                         <Col className="box border border-dark">1</Col>
@@ -35,13 +35,13 @@ export default function CreateRoom() {
                     </Row>
 
                 </Container> */}
-        <div className="row p-2 m-2">
-          <div className="col options-box card" onClick={handleClick} id='classic'>classic</div>
-          <div className= "col options-box card" onClick={handleClick} id='knockout'>knockout</div>
+      <div className="row p-2 m-2">
+          <div className="col options-box card fs-1 text game-mode-options-classic" onClick={handleClick} id='classic'>Classic</div>
+          <div className= "col options-box card fs-1 text game-mode-options-knockout" onClick={handleClick} id='knockout'>Knockout</div>
         </div>
         <div className="row p-2 m-2">
-          <div className="col options-box card" onClick={handleClick} id='quickfire'>quickfire</div>
-          <div className="col options-box card" onClick={handleClick} id='custom'>custom</div>
+          <div className="col options-box card fs-1 text game-mode-options-quickfire" onClick={handleClick} id='quickfire'>Quickfire</div>
+          <div className="col options-box card fs-1 text game-mode-options-custom" onClick={handleClick} id='custom'>Custom</div>
         </div>
       </Container>
     </div> : <Redirect to='/selectCategory'/>
--- src/pages/SelectCategory/index.js
+++ src/pages/SelectCategory/index.js
@@ -4,6 +4,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { addCategory } from "../../actions";
 import { Redirect } from "react-router";
 import { getQuestions } from "../../actions";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faLaptop } from '@fortawesome/free-solid-svg-icons'
+import { faPaw } from '@fortawesome/free-solid-svg-icons'
+import { faHistory } from '@fortawesome/free-solid-svg-icons'
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons'
+import { faAtlas } from '@fortawesome/free-solid-svg-icons'
+import { faFlask } from '@fortawesome/free-solid-svg-icons'
+import { faLandmark } from '@fortawesome/free-solid-svg-icons'
+
+
+
 export default function SelectCategory() {
   const dispatch = useDispatch();
   const [redirect, setRedirect] = useState(false)
@@ -36,25 +48,41 @@ export default function SelectCategory() {
         <div className="row p-2 m-2">
             
           <div className="col options-box-categories card">
-          <h1>select category</h1>
+          <h1>Select Category</h1>
             <div className="row">
-              <div className="col category-box card" id='9' onClick={handleClick}>General Knowledge</div>
-              <div className="col category-box card" id='17' onClick={handleClick}>Science & Nature</div>
+              <div className="col category-box card border-white" id='9' onClick={handleClick}>
+                <div><FontAwesomeIcon icon={faSearch} size="4x" className="search"/></div> 
+                  General Knowledge</div>
+              <div className="col category-box card border-white" id='17' onClick={handleClick}>
+                <div><FontAwesomeIcon icon={faFlask} size="4x" className="science"/></div>
+                Science</div>
             </div>
             <div className="row">
-              <div className="col category-box card" id='18' onClick={handleClick}>Science: Computers</div>
-              <div className="col category-box card" id='21' onClick={handleClick}>Sports</div>
+              <div className="col category-box card border-white" id='18' onClick={handleClick}>
+                <div><FontAwesomeIcon icon={faLaptop} size="4x" className="computers"/></div>
+                Computers</div>
+              <div className="col category-box card border-white" id='21' onClick={handleClick}>
+                <div><FontAwesomeIcon icon={faDumbbell} size="4x" className="sports"/></div>
+              Sports</div>
             </div>
             <div className="row">
-              <div className="col category-box card" id='27' onClick={handleClick}>Animals</div>
-              <div className="col category-box card" id='22' onClick={handleClick}>Geography</div>
+              <div className="col category-box card border-white" id='27' onClick={handleClick}>
+                <div><FontAwesomeIcon className="fa-lg" icon={faPaw} size="4x" className="animals"/></div>
+                Animals</div>
+              <div className="col category-box card border-white" id='22' onClick={handleClick}>
+                <div><FontAwesomeIcon icon={faAtlas} size="4x" className="geography"/></div>
+                Geography</div>
             </div>
             <div className="row">
-            <div className = 'col category-box card ' id='23' onClick={handleClick}>History</div>
-                  <div className = 'col category-box card' id='24' onClick={handleClick}>Politics</div>
+            <div className = 'col category-box card border-white' id='23' onClick={handleClick}>
+              <div><FontAwesomeIcon icon={faHistory} size="4x" className="history"/></div>
+            History</div>
+                  <div className = 'col category-box card border-white' id='24' onClick={handleClick}>
+                    <div><FontAwesomeIcon icon={faLandmark} size="4x" className="politics"/></div>
+                    Politics</div>
             </div>
             <div className="row">
-            <div className = 'col category-box card'>custom</div>
+            <div className = 'col category-box card'>Custom</div>
                   
             </div>
           </div>
