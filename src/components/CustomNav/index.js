298e8a69f78b6e84c98b442ece39e29f51bbe6b8
Callum <80120239+cmr33-cloud@users.noreply.github.com>
2021-10-28 19:07:23 +0100

    title/logan/slogan

--- src/components/CustomNav/index.js
+++ src/components/CustomNav/index.js
@@ -8,7 +8,7 @@ export default function CustomNav() {
                     <Container className = 'd-flex flex-row align-items-center justify-content-center'>
                     <h2 className = 'custom-nav-text'>Inquizitive</h2>
                     <br/>
-                    <p className = 'custom-nav-text'>catchy slogan</p>
+                    <p className = 'custom-nav-text'>the more you know, the more you grow</p>
                     </Container>
                     </Navbar.Brand>
                 
